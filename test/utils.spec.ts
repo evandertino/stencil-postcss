8ffba91213b1ce708efdb05660648372cadb1c2d
Evander Otieno <evandertino@outlook.com>
2020-04-08 20:17:08 +0300

    fix: update type declarations, allow context to be undefined

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -1,9 +1,7 @@
 import * as d from '../src/declarations';
 import * as util from '../src/util';
 
-
 describe('getRenderOptions', () => {
-
   const sourceText = 'body { color: blue; }';
   const fileName = '/some/path/file-name.scss';
   const context: d.PluginCtx = {
@@ -12,14 +10,13 @@ describe('getRenderOptions', () => {
       srcDir: '/Users/my/app/src/',
     },
     fs: {},
-    diagnostics: []
+    diagnostics: [],
   };
   const dummyPlugin = () => null;
 
-
   it('should inject plugins array and not change input options', () => {
     const input: d.PluginOptions = {
-      plugins: [dummyPlugin]
+      plugins: [dummyPlugin],
     };
     const output = util.getRenderOptions(input, sourceText, context);
     expect(output.plugins).toHaveLength(1);
@@ -30,7 +27,7 @@ describe('getRenderOptions', () => {
 
   it('should inject global postcss array and not change input options or include globals in output opts', () => {
     const input: d.PluginOptions = {
-      injectGlobalPaths: ['/my/global/variables.pcss']
+      injectGlobalPaths: ['/my/global/variables.pcss'],
     };
     const output = util.getRenderOptions(input, sourceText, context);
     expect(output.data).toBe(`@import "/my/global/variables.pcss";body { color: blue; }`);
@@ -45,11 +42,18 @@ describe('getRenderOptions', () => {
     expect(output.plugins).toHaveLength(0);
   });
 
+  it('should not break with undefined context', () => {
+    const input: d.PluginOptions = {
+      injectGlobalPaths: ['./my/global/variables.pcss'],
+    };
+
+    expect(() => {
+      const output = util.getRenderOptions(input, undefined, undefined);
+    }).not.toThrow();
+  });
 });
 
-
 describe('usePlugin', () => {
-
   it('should use the plugin for .pcss file', () => {
     const fileName = 'my-file.pcss';
     expect(util.usePlugin(fileName)).toBe(true);
@@ -69,15 +73,12 @@ describe('usePlugin', () => {
     const fileName = 'my-file.less';
     expect(util.usePlugin(fileName)).toBe(false);
   });
-
 });
 
 describe('createResultsId', () => {
-
   it('should change pcss the extension to be css', () => {
     const input = '/my/path/my-file.pcss';
     const output = util.createResultsId(input);
     expect(output).toBe('/my/path/my-file.css');
   });
-
 });
